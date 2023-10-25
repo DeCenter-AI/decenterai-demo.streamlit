bae34dc05c493589e1ebb6a9bdc00b63dd11237a
Dinesh Jonnalagadda <106725225+Dineshjnld@users.noreply.github.com>
2023-10-25 06:21:30 +0530

    Update head.py

--- views/head.py
+++ views/head.py
@@ -70,7 +70,24 @@ def head_v3():
     col2.image(public.logo, width=400)
 
     #st.sidebar.success("Load complete")
+    st.sidebar.text("Download the Samples here!!")
+    def create_download_link(file_path, file_name):
+        with open(file_path, 'rb') as file:
+            file_data = file.read()
+        b64 = base64.b64encode(file_data).decode() 
+        href = f'<a href="data:application/octet-stream;base64,{b64}" download="{file_name}.zip">{file_name}</a>'
+        return href
 
+    file_options = {
+    "Sample1": "samples/demos/simple-linear-regression.zip",
+    "Sample2": "samples/demos/multiple-linear-regression.zip",
+    "Sample3": "samples/demos/boston-housing-price-prediction.zip",
+    "Sample4": "samples/demos/headbrain.zip"
+    }
+
+    for project, file_path in file_options.items():
+        if os.path.exists(file_path):
+            st.sidebar.markdown(create_download_link(file_path, project), unsafe_allow_html=True)
 
 def head_v4():
     # to be tested
