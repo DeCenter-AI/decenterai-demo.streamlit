e45f6a38e88665f58b739752bd0654bc4a4b8b94
Dinesh Jonnalagadda <106725225+Dineshjnld@users.noreply.github.com>
2023-10-25 06:37:10 +0530

    Update head.py

--- views/head.py
+++ views/head.py
@@ -1,13 +1,13 @@
 import logging
-import os
-import base64
+
 import streamlit as st
 from streamlit.commands.page_config import (
     REPORT_A_BUG_KEY,
     ABOUT_KEY,
     GET_HELP_KEY,
 )
-
+import os
+import base64
 import public
 from config.constants import GITHUB_REPORT_BUG, APP_ABOUT, GITHUB_DISCUSSION_QA
 
@@ -48,25 +48,6 @@ def head_v3():
         initial_sidebar_state="collapsed",
     ) 
     st.sidebar.title("Demo App V3")
-    st.sidebar.text("Download the Samples here!!")
-    def create_download_link(file_path, file_name):
-        with open(file_path, 'rb') as file:
-            file_data = file.read()
-        b64 = base64.b64encode(file_data).decode() 
-        href = f'<a href="data:application/octet-stream;base64,{b64}" download="{file_name}.zip">{file_name}</a>'
-        return href
-
-    file_options = {
-    "Sample1": "samples/demos/simple-linear-regression.zip",
-    "Sample2": "samples/demos/multiple-linear-regression.zip",
-    "Sample3": "samples/demos/boston-housing-price-prediction.zip",
-    "Sample4": "samples/demos/headbrain.zip"
-    }
-
-    for project, file_path in file_options.items():
-        if os.path.exists(file_path):
-            st.sidebar.markdown(create_download_link(file_path, project), unsafe_allow_html=True)
-
     st.markdown(public.button_styles_css, unsafe_allow_html=True)
 
     st.sidebar.write(
@@ -90,7 +71,27 @@ def head_v3():
     col2.image(public.logo, width=400)
 
     #st.sidebar.success("Load complete")
-    
+    st.sidebar.text("Download the Samples here!!")
+    def create_download_link(file_path, file_name):
+        with open(file_path, 'rb') as file:
+            file_data = file.read()
+        b64 = base64.b64encode(file_data).decode() 
+        href = f'<a href="data:application/octet-stream;base64,{b64}" download="{file_name}.zip">{file_name}</a>'
+        return href
+
+    file_options = {
+    "Sample1": "samples/demos/simple-linear-regression.zip",
+    "Sample2": "samples/demos/multiple-linear-regression.zip",
+    "Sample3": "samples/demos/boston-housing-price-prediction.zip",
+    "Sample4": "samples/demos/headbrain.zip"
+    }
+
+    for project, file_path in file_options.items():
+        if os.path.exists(file_path):
+            st.sidebar.markdown(create_download_link(file_path, project), unsafe_allow_html=True)
+
+
+
 def head_v4():
     # to be tested
     st.markdown(
